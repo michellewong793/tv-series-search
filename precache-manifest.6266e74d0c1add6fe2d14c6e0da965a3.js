e21a0abd65646ddb27501da8144fc0b1324d36dc
Michelle Wong <michellewong793@gmail.com>
2019-08-15 13:43:07 -0700

    Updates

---
+++ precache-manifest.6266e74d0c1add6fe2d14c6e0da965a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a3e60746ce81285a510",
+    "url": "/tv-series-search/static/js/runtime~main.0a3e6074.js"
+  },
+  {
+    "revision": "5219fed32ed67de02fba",
+    "url": "/tv-series-search/static/js/main.5219fed3.chunk.js"
+  },
+  {
+    "revision": "6b3b2660436687524cae",
+    "url": "/tv-series-search/static/js/2.6b3b2660.chunk.js"
+  },
+  {
+    "revision": "5219fed32ed67de02fba",
+    "url": "/tv-series-search/static/css/main.4f894eb0.chunk.css"
+  },
+  {
+    "revision": "faad9d2a224bc8f15d8ddada71bd4782",
+    "url": "/tv-series-search/index.html"
+  }
+];
